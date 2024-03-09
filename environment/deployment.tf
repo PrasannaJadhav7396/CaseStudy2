b907f28b5d9023fa5755709b3c57c2ea35bef666
Prasanna Jadhav <PrasannaJ@citiustech.com>
2024-03-09 23:31:10 +0530

    Small fix

--- environment/deployment.tf
+++ environment/deployment.tf
@@ -32,14 +32,28 @@ resource "aws_lb" "app" {
   security_groups    = ["sg-90120934239"] 
   subnets            = ["subnet-453231352"]         
 }
- 
+
+resource "aws_acm_certificate" "cert" {
+  count             = var.env_enabled  
+  domain_name       = "casestudy2.com"
+  validation_method = "DNS"
+
+  tags = {
+    Environment = "Dev"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 resource "aws_lb_listener" "https" {
   count             = var.env_enabled
   load_balancer_arn = aws_lb[0].app.arn
   port              = var.listener_port
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = "certificate_arn" # Replace with your SSL certificate ARN
+  certificate_arn   = aws_acm_certificate.cert[0].arn
  
   default_action {
     type             = "forward"
@@ -52,5 +66,5 @@ resource "aws_lb_target_group" "app" {
   name     = "${var.environment_name}-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "your_vpc_id" # Replace with your VPC ID
+  vpc_id   = "vpc-324235"
 }
