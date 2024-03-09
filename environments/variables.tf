f1e7b9876930250b8eee5e6cd96feafb54e5fe53
Prasanna Jadhav <PrasannaJ@citiustech.com>
2024-03-10 00:05:57 +0530

    Small fix

---
+++ environments/variables.tf
@@ -0,0 +1,38 @@
+variable "environment_name" {
+}
+
+variable "instance_type" {
+ default = "t2.micro"
+}
+
+variable "ami_id" {
+ default = "ami-0440d3b780d96b29d"
+} 
+
+variable "desired_capacity" {
+ default = 2
+}
+
+variable "max_size" {
+ default = 3
+}
+
+variable "min_size" {
+ default = 1
+}
+
+variable "listener_port" {
+ default = "443"
+}
+
+variable "alb_priority" {
+}
+
+variable "alb_path_pattern" {
+ default = "/*"
+}
+
+variable "env_enabled" {
+ default = false
+}
+
